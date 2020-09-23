70bddf3db7ee7bd25666f5ef008826ea927ad66c
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-09-23 19:35:40 +0700

    feature(ContactsApp): добавлено перечисление CheckType

---
+++ ContactsApp/ContactsApp/PhoneNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp
+{
+    /// <summary>
+    /// Класс номера телефона, хранящий номер телефона контакта
+    /// </summary>
+    public class PhoneNumber
+    {
+        private int _number;
+        public int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                const int numbersLength = 11;
+                
+            }
+        }
+    }
+}
---
+++ ContactsApp/ContactsApp/ValueValidator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ContactsApp
+{
+    public static class ValueValidator
+    {
+        public static bool IsNumbersLengthIs(int number, int digitCount)
+        {
+            int digitCounter = 0;
+            while (number > 0)
+            {
+                digitCounter++;
+                number /= 10;
+            }
+
+            return digitCounter == digitCount;
+        }
+
+        public static bool IsFirstDigitIs7(int number)
+        {
+            if(number / 10000000000 == 7)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static void IsCorrectValue()
+        {
+
+        }
+    }
+}
