e0cc881a3448e2cf985032373b612dd6da6ad713
Kabanova Ekaterina <katovskaya009@gmail.com>
2020-10-05 13:41:33 +0700

    docs(ProjectManager): добавлены xml-комментарии

--- ContactsApp/ContactsApp/ICloneable.cs
+++ ContactsApp/ContactsApp/ICloneable.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ContactsApp
 {
+   
     public interface ICloneable
     {
         object Clone();
--- ContactsApp/ContactsApp/ProjectManager.cs
+++ ContactsApp/ContactsApp/ProjectManager.cs
@@ -6,14 +6,37 @@ using Newtonsoft.Json;
 
 namespace ContactsApp
 {
+	/// <summary>
+	/// Класс "Управление проектом" предназначен
+	/// для организации сериализации и десериализации
+	/// объектов класса "Проект"
+	/// </summary>
 	public static class ProjectManager
 	{
+		/// <summary>
+		/// Поле "Имя файла" хранит имя файла,
+		/// в который будет выполняться сохранение объектов
+		/// </summary>
 		private const string FILE_NAME = "ContactsApp.notes";
+		/// <summary>
+		/// Поле "Папка" хранит путь к каталогу,
+		/// выполняющего функции общего репозитория
+		/// для данных приложения текущего перемещающегося пользователя,
+		/// в котором будет создан каталог ContactsApp
+		/// </summary>
 		private static readonly string _folder = Environment.GetFolderPath(
 				Environment.SpecialFolder.ApplicationData) +
 			"\\ContactsApp\\";
+		/// <summary>
+		/// Поле "Путь" хранит абсолютный путь к файлу,
+		/// в который будет выполняться сохранение объектов
+		/// </summary>
 		private static readonly string _path = _folder + FILE_NAME;
 
+		/// <summary>
+		/// Создает объект класса ProjectManager
+		/// и файл для сериализации
+		/// </summary>
 		static ProjectManager()
 		{
 			if (!Directory.Exists(_folder))
@@ -26,6 +49,10 @@ namespace ContactsApp
 			}
 		}
 
+		/// <summary>
+		/// Метод выполняет сериализацию объекта
+		/// </summary>
+		/// <param name="project">Сериализуемый объект</param>
 		public static void SaveProject(Project project)
 		{
             using (StreamWriter file = new StreamWriter(
@@ -35,6 +62,10 @@ namespace ContactsApp
             }
         }
 
+		/// <summary>
+		/// Метод выполныет десериализацию объекта
+		/// </summary>
+		/// <returns>Десериализованный объект</returns>
 		public static Project ReadProject()
 		{
 			Project project = new Project();
